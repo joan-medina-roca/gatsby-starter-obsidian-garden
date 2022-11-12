c11704923e7c6b6f37117f6afd4d0f71a68e2efc
joan medina <joan.medina.roca.wordpress@gmail.com>
2022-11-12 10:09:09 +0100

    canvis single post

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -18,13 +18,11 @@ const PostTemplate = ({ data }) => {
     <MdxProvider>
       <Layout>
         {/* <SEO postNode={data.mdx} postSEO postPath={data.mdx.fields.slug} /> */}
-        <small className="text-sm font-bold uppercase tracking-tight">
-          {post.date}
-        </small>
+        <small className="text-sm font-bold uppercase tracking-tight">{post.date}</small>
         <article className="my-6 prose prose-green lg:prose-xl">
           <MDXRenderer>{postNode.body}</MDXRenderer>
         </article>
-        <h5 className="uppercase font-bold mb-4">Topics</h5>
+        <h5 className="uppercase font-bold mb-4">Temes</h5>
         <ul className="flex flex-row gap-4 mb-4">
           {tags &&
             tags.map((tag) => (
@@ -33,10 +31,8 @@ const PostTemplate = ({ data }) => {
               </li>
             ))}
         </ul>
-        <h5 className="uppercase font-bold mb-4">Maturity</h5>
-        <small className="bg-green-50 text-green-700 text-xs tracking-normal uppercase rounded-lg px-2 py-1 my-2">
-          {post.stage}
-        </small>
+        <h5 className="uppercase font-bold mb-4">Maduresa</h5>
+        <small className="bg-green-50 text-green-700 text-xs tracking-normal uppercase rounded-lg px-2 py-1 my-2">{post.stage}</small>
       </Layout>
     </MdxProvider>
   );
