e27350cceeca598aad6289f717137bb176d6b204
joan medina <joan.medina.roca.wordpress@gmail.com>
2022-11-12 09:45:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,11 @@ module.exports = {
   pathPrefix: "",
   SITE_TITLE: "Obsidian Garden",
   SITE_SUBTITLE: "Gatsby Starter Made for Obsidian Users",
-  SITE_DESCRIPTION:
-    "A Gatsby starter template to publish Obsidian vault for free",
+  SITE_DESCRIPTION: "A Gatsby starter template to publish Obsidian vault for free",
   KEYWORDS: "Gatsby, Starter, Obsidian, Digial Garden",
-  HERO_TITLE:
-    "Free Gatsby starter template to host your own digital garden for free, created for Obsidian knowledge base.",
-  HERO_SUBTITLE: "Designed by JUXT Design",
-  COPYRIGHT: "JUXT Design",
+  HERO_TITLE: "Si em donessin un cèntim per cada pensament que no he escrit ni publicat enlloc, seria milionari.",
+  HERO_SUBTITLE: "Joan Medina Roca",
+  COPYRIGHT: "Joan Medina Roca",
   LOCALE: "en",
   POST_PER_PAGE: 6,
   MENU: [
@@ -30,14 +28,6 @@ module.exports = {
     {
       label: "About",
       path: "/about/",
-    },
-    {
-      label: "Changelog",
-      path: "/changelog/",
-    },
-    {
-      label: "Github",
-      path: "https://github.com/juxtdesigncc/gatsby-starter-obsidian-garden",
     },
   ],
   AUTHOR: {
