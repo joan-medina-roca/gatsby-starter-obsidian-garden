3cffbc519e6249cc8262e18564ded861d98531a6
joan medina <joan.medina.roca.wordpress@gmail.com>
2022-11-12 14:47:35 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,15 +17,6 @@ export default function Header() {
                 <Link to={item.path}>{item.label}</Link>
               </li>
             ))}
-            <li>
-              <Link
-                to="https://github.com/juxtdesigncc/gatsby-starter-obsidian-garden"
-                target="_blank"
-                className="px-4 py-2 bg-gray-800 rounded-full text-white hover:bg-gray-600 transition-all ease-in-out duration-300 hover:text-white uppercase"
-              >
-                Try Now
-              </Link>
-            </li>
           </ul>
         </nav>
       </Container>
